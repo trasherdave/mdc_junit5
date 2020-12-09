cb81985478f693cff807e308196e685a97f7de3b
David Rodrigues <56417462+trasherdave@users.noreply.github.com>
2020-12-09 18:18:02 -0300

    Complemento no código

--- src/main/java/com/david/mdc_junit5/MathUtil.java
+++ src/main/java/com/david/mdc_junit5/MathUtil.java
@@ -7,9 +7,31 @@ package com.david.mdc_junit5;
 public class MathUtil {
 
     public static int mdc(int a, int b) {
+        // Propriedade 7
+        a = Math.abs(a);
+        b = Math.abs(b);
+
+        // Propriedade 6
+        final int maior = Math.max(a, b);
+        b = Math.min(a, b);
+        a = maior;
+
+        //Propriedade 1
         if (b > 0 && a % b == 0) {
             return b;
         }
+
+        //Propriedade 3
+        if (b == 0) {
+            return Math.abs(a);
+        }
+        //Propriedade 5
+        if (a % b != 0) {
+            return 1;
+        }
+        
+        
+
         return -1;
     }
 }
